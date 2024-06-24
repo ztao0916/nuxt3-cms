a6de913f1dd48bdd6079c2c63dfe5d4853dbdfa8
ztao <649949135@qq.com>
2024-06-24 13:57:24 +0800

    feat: 引入element-plus

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div>首页</div>
+    <el-button @click="ElMessage('hello')">button</el-button>
+    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+    <LazyElButton type="warning">lazy button</LazyElButton>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
