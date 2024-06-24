0b46c7b113cc4b1860d42ccb918f3499cd3e0f84
ztao <649949135@qq.com>
2024-06-24 20:09:17 +0800

    feat: 测试分页逻辑-准确

---
+++ components/Items.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  defineProps<{
+    items: any[];
+  }>();
+</script>
+
+<template>
+  <div v-for="item in items">品牌{{ item.brand }}</div>
+</template>
+
+<style lang="css" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,30 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { useRoute } from "vue-router";
+
+  interface Post {
+    [key: string]: any;
+  }
+
+  const route = useRoute();
+  console.log(route.params);
+  const page = ref(1);
+  const { data: posts, refresh } = await useAsyncData("posts", () =>
+    useClientRequest<Post>("/lastRelease", {
+      server: false,
+      query: {
+        page: page.value,
+        limit: 2,
+      },
+    })
+  );
+  console.log("data:post", posts.value?.data, posts.value?.total);
+  //分页
+  const handleCurrentChange = (val: number) => {
+    console.log(`current page: ${val}`);
+    page.value = val;
+    refresh();
+  };
+</script>
 
 <template>
   <div>
@@ -6,6 +32,16 @@
     <el-button @click="ElMessage('hello')">button</el-button>
     <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
     <LazyElButton type="warning">lazy button</LazyElButton>
+    <!-- 渲染post.data -->
+    <Items :items="posts?.data"></Items>
+    <el-pagination
+      layout="pager, next"
+      next-text="下一页"
+      :total="posts?.total"
+      :default-page-size="2"
+      v-model:current-page="page"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
